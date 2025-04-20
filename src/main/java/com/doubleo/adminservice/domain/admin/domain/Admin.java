42d7c4be49af433e7464991b7e0c481087e833a3
willjsw <sunwoo1137@gmail.com>
2025-04-21 00:21:32 +0900

    KW-121/chore: project init

---
+++ src/main/java/com/doubleo/adminservice/domain/admin/domain/Admin.java
@@ -0,0 +1,12 @@
+package com.doubleo.adminservice.domain.admin.domain;
+
+import com.doubleo.adminservice.domain.common.model.BaseTimeEntity;
+import jakarta.persistence.*;
+
+@Entity
+public class Admin extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private Long id;
+}
