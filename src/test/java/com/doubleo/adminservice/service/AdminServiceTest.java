f0e0bb9f921218e25f222a872b7b19c8f0d8a0e1
willjsw <sunwoo1137@gmail.com>
2025-05-07 10:50:47 +0900

    KW-278/refactor: unit test tenantValidator Mockup

--- src/test/java/com/doubleo/adminservice/service/AdminServiceTest.java
+++ src/test/java/com/doubleo/adminservice/service/AdminServiceTest.java
@@ -11,6 +11,7 @@ import com.doubleo.adminservice.domain.admin.repository.AdminRepository;
 import com.doubleo.adminservice.domain.admin.service.AdminServiceImpl;
 import com.doubleo.adminservice.global.exception.CommonException;
 import com.doubleo.adminservice.global.exception.errorcode.AdminErrorCode;
+import com.doubleo.adminservice.global.util.TenantValidator;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -30,6 +31,8 @@ public class AdminServiceTest {
     @Mock private AdminRepository adminRepository;
 
     @Mock private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Mock private TenantValidator<Admin> tenantValidator;
 
     private final String username = "test@test.com";
     private final String password = "password";
@@ -51,6 +54,7 @@ public class AdminServiceTest {
         void 관리자정보_조회하면_정상적으로_반환된다() {
             // given
             given(adminRepository.findById(1L)).willReturn(Optional.of(admin));
+            given(tenantValidator.validateTenant(admin)).willReturn(admin);
 
             // when
             AdminInfoResponse response = adminService.getAdminInfo(admin.getId());
@@ -73,6 +77,8 @@ public class AdminServiceTest {
             String encodedNewPassword = "encodedNew";
 
             given(adminRepository.findById(1L)).willReturn(Optional.of(admin));
+            given(tenantValidator.validateTenant(admin)).willReturn(admin);
+
             given(bCryptPasswordEncoder.matches(password, "encoded")).willReturn(true);
             given(bCryptPasswordEncoder.encode(newPassword)).willReturn(encodedNewPassword);
 
@@ -87,6 +93,7 @@ public class AdminServiceTest {
         void 기존_비밀번호_유효하지_않으면_오류_발생한다() {
             // given
             given(adminRepository.findById(1L)).willReturn(Optional.of(admin));
+            given(tenantValidator.validateTenant(admin)).willReturn(admin);
             given(bCryptPasswordEncoder.matches("wrongPassword", "encoded")).willReturn(false);
 
             // when & then
@@ -104,6 +111,7 @@ public class AdminServiceTest {
         void 기존_비밀번호와_신규_비밀번호가_동일하면_오류_발생한다() {
             // given
             given(adminRepository.findById(1L)).willReturn(Optional.of(admin));
+            given(tenantValidator.validateTenant(admin)).willReturn(admin);
             given(bCryptPasswordEncoder.matches(password, "encoded")).willReturn(true);
 
             // when & then
